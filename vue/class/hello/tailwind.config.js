38736e485de6bef5e855f5e4a5a9d9832fc0747f
love-yuri <yuri2078170658@gmail.com>
2023-10-29 10:55:13 +0800

    修改默认样式-增加tailwindcss

---
+++ vue/class/hello/tailwind.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2023-10-28 19:04:09
+ * @LastEditTime: 2023-10-28 19:04:26
+ * @Description:taildwindcsss配置文件
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+};
