0ad75b7ab9e3fadf7a432502620d12c1c2cb6874
love-yuri <yuri2078170658@gmail.com>
2024-05-21 23:39:26 +0800

    添加了KNN分类算法，优化了数据处理方式，并添加了测试数据和图表展示。

--- python/machine-learning/knn/海伦约会.py
+++ python/machine-learning/knn/海伦约会.py
@@ -1,23 +1,41 @@
 '''
 Author: love-yuri yuri2078170658@gmail.com
 Date: 2024-04-18 20:12:46
-LastEditTime: 2024-05-07 08:09:09
+LastEditTime: 2024-05-21 23:37:58
 Description: 海伦约会
 '''
 import numpy as np
 import os
 import re
-from yuri_util import info, get_script_dir
+from yuri_util import info, Utils
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
-basePath = get_script_dir(__file__)
+basePath = Utils.get_script_dir(__file__)
 
+# 0：每年获得的飞行常客里程数
+# 1：玩视频游戏所消耗时间百分比
+# 2：每周消费的冰淇淋公升数
 datas = []
 labels = []
 
+# 标签map
 labelMap = {
   'didntLike': 1,
   'smallDoses': 2,
   'largeDoses': 3
+}
+
+resultLabelMap = {
+  1: '不喜欢的人',
+  2: '魅力一般的人',
+  3: '极具魅力的人'
+}
+
+colorMap = {
+  1: 'black', 
+  2: 'orange', 
+  3: 'red'
 }
 
 # 读取数据
@@ -27,34 +45,96 @@ with open(f'{basePath}/datingTestSet.txt', 'r') as file:
     datas.append(values[0:3])
     labels.append(labelMap[values[-1]])
 
-datas = np.array(datas).astype(float) # 数据集
-labels = np.array(labels) # 标签集
-
+datas: np.ndarray = np.array(datas).astype(float) # 数据集
+labels: np.ndarray = np.array(labels) # 标签集
 
 # 计算k个最近邻的标签
 def knn_classify(x: np.ndarray, k):
-  # 数据归一化
+  # 数据归一化 将每个数据 / (最大值 - 最小值)
   global datas, labels
   minVals = datas.min(0) # 获取最小值
   maxVals = datas.max(0) # 获取最大值
   ranges = maxVals - minVals # 获取范围
 
-  datas = datas - np.tile(minVals, (datas.shape[0], 1)) # 减去最小值
-  datas = datas / np.tile(ranges, (datas.shape[0], 1)) # 除以范围
+  datas_r = datas - np.tile(minVals, (datas.shape[0], 1)) # 减去最小值
+  datas_r = datas / np.tile(ranges, (datas.shape[0], 1)) # 除以范围
 
   x = x - minVals
   x = x / ranges
 
   # 一共有n行数据
-  row = datas.shape[0]
-  dis = np.tile(x, (row, 1)) - datas
+  row = datas_r.shape[0]
+  dis = datas_r - np.tile(x, (row, 1))
   dis = np.sqrt(np.sum(np.square(dis), axis=1))
   sortIndex = np.argsort(dis)
   result = {}
   for i in range(k):
     result[labels[sortIndex[i]]] = result.get(labels[sortIndex[i]], 0) + 1
   # 排序
+  info() << "结果统计: " << result
   return max(result.items(), key=lambda x: x[1])[0]
 
-# 测试
-info() << knn_classify(np.array([2, 2, 1]), 10)
+
+# 设置新的数据 --------- 测试
+testVal = np.array([40000, 10, 1])
+val = knn_classify(testVal, 100)
+
+# 公有图例
+didntLike = mlines.Line2D([], [], color='black', marker='.',markersize=6, label='不喜欢的人')
+smallDoses = mlines.Line2D([], [], color='orange', marker='.',markersize=6, label='魅力一般的人')
+largeDoses = mlines.Line2D([], [], color='red', marker='.',markersize=6, label='极具魅力的人')
+testRes = mlines.Line2D([], [], color='gray', marker='.',markersize=14, label=resultLabelMap.get(val))
+
+plt.rcParams["font.family"]="SimHei"
+pig = plt.figure(figsize=(8, 8))
+
+baseSize = [10 for k in labels]
+baseColor = [colorMap.get(k) for k in labels]
+
+# 添加最后的测试数据
+baseSize.append(140)
+baseColor.append('gray') # 测试数据颜色
+
+datas = np.append(datas, [testVal], axis=0)
+labels = np.append(labels, val)
+
+ax = pig.add_subplot(221)
+ax.scatter(datas[:, 0], datas[:, 1], s=baseSize, c=baseColor)
+ax.set_title('每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', fontsize=7)
+ax.set_xlabel('每年获得的飞行常客里程数', fontsize=8)
+ax.set_ylabel('玩视频游戏所消耗时间百分比', fontsize=8)
+
+#添加图例
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+
+ax = pig.add_subplot(222)
+ax.scatter(datas[:, 0], datas[:, 2], s=baseSize, c=baseColor)
+ax.set_title('每年获得的飞行常客里程数与每周消费的冰淇淋公升数', fontsize=7)
+ax.set_xlabel('每年获得的飞行常客里程数', fontsize=8)
+ax.set_ylabel('每周消费的冰淇淋公升数', fontsize=8)
+
+#添加图例
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+
+ax = pig.add_subplot(223)
+ax.scatter(datas[:, 1], datas[:, 2], s=baseSize, c=baseColor)
+ax.set_title('玩视频游戏所消耗时间占比与每周消费的冰淇淋公升数', fontsize=7)
+ax.set_xlabel('玩视频游戏所消耗时间百分比', fontsize=8)
+ax.set_ylabel('每周消费的冰淇淋公升数', fontsize=8)
+
+#添加图例
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+ax.legend(handles=[didntLike, smallDoses, largeDoses, testRes])
+
+ax = pig.add_subplot(224)
+
+
+plt.show()
--- python/machine-learning/knn/电影鉴定.py
+++ python/machine-learning/knn/电影鉴定.py
@@ -1,15 +1,18 @@
 '''
 Author: love-yuri yuri2078170658@gmail.com
 Date: 2024-04-05 22:46:20
-LastEditTime: 2024-04-18 19:57:24
+LastEditTime: 2024-05-21 19:13:14
 Description: 电影鉴定 knn-临近算法学习
 '''
 import numpy as np
-from yuri_log import info, error
+import matplotlib.pyplot as plt
+from yuri_util import info, error
 import operator
 
 def createData():
   # 创建数据集
+  # 第一个参数表示 动作方面镜头数
+  # 第二个参数表示 爱情方面镜头数
   group = np.array([[1,101],[5,89],[108,5],[115,8]])
 
   # 创建标签集
@@ -30,27 +33,33 @@ def classify0(inX: np.ndarray, dataSet: np.ndarray, labels: np.ndarray, k):
     distances = sqDistances**0.5
     #返回distances中元素从小到大排序后的索引值
     sortedDistIndices = distances.argsort()
-    info() << sortedDistIndices
+    info() << '和各点的距离 -> ' << distances
+    info() << '排序后的结果 -> ' << sortedDistIndices
     #定一个记录类别次数的字典
     classCount = {}
     for i in range(k):
       #取出前k个元素的类别
       voteIlabel = labels[sortedDistIndices[i]]
-      info() << sortedDistIndices[i]
       #dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
       #计算类别次数
       classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
 
-    #reverse降序排序字典
+    # reverse降序排序字典
     sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
     info() << sortedClassCount
-    #返回次数最多的类别,即所要分类的类别
+    # 返回次数最多的类别,即所要分类的类别
     return sortedClassCount[0][0]
 
 if __name__ == '__main__':
   group, labels = createData()
-  test = [101,200]
+  test = [101, 100]
   #kNN分类
   test_class = classify0(test, group, labels, 3)
+  xpoint = [p[0] for p in group]
+  ypoint = [p[1] for p in group]
+  xpoint.append(test[0])
+  ypoint.append(test[1])
+  plt.plot(xpoint, ypoint, 'o')
   #打印分类结果
-  print(test_class)
+  info() << (test_class)
+  plt.show()
