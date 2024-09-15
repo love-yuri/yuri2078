e2f9d8d6352f948a95622cea1a45c4096bdc7049
love-yuri <yuri2078170658@gmail.com>
2024-09-15 23:46:32 +0800

    feat: 修改了leetcode题目26的解决方案

--- leetcode/26.删除有序数组中的重复项.cpp
+++ leetcode/26.删除有序数组中的重复项.cpp
@@ -9,19 +9,17 @@
 using namespace std;
 class Solution {
 public:
-	int removeDuplicates(vector<int>& nums)
-	{
-		// 默认第一个元素是绝对不会重复的
-		int k = 1, size = nums.size();
-		// 循环判断每个元素是否和他前一个元素相同
-		for (int i = 1; i < size; i++) {
-			// 不同就直接添加进去
-			if (nums[i] != nums[i - 1]) {
-				nums[k++] = nums[i];
-            }
-		}
-		return k;
+  int removeDuplicates(vector<int> &nums) {
+    // 默认第一个元素是绝对不会重复的
+    int k = 1, size = nums.size();
+    // 循环判断每个元素是否和他前一个元素相同
+    for (int i = 1; i < size; i++) {
+      // 不同就直接添加进去
+      if (nums[i] != nums[i - 1]) {
+        nums[k++] = nums[i];
+      }
     }
+    return k;
+  }
 };
 // @lc code=end
-
