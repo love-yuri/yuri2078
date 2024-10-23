94320f3f27fabfcbcba83ff9120354548567c962
love-yuri <yuri2078170658@gmail.com>
2024-10-23 16:27:57 +0800

    feat(test.cpp): 修改测试文件，实现新的功能

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,27 +1,16 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-06-15 21:45:04
- * @LastEditTime: 2024-06-15 22:47:31
+ * @LastEditTime: 2024-10-21 11:04:14
  * @Description:
  */
 
-#include <iostream>
-#include <functional>
-#include <vector>
+#include <yuri/yuri_log.hpp>
 
-using std::vector;
-
-template <typename U, typename F>
-vector<int> &operator|(U &&v, F f) {
-  for (auto &x : v) {
-    f(x);
-  }
-  return v;
-}
+// #include <iostream>
+// #define yinfo std::cout
 
 int main() {
-  std::vector v{1, 2, 3};
-  std::function f{[](const int &i) { std::cout << i << ' '; }};
-  auto f2 = [](int &i) { i *= i; };
-  v | f2 | f;
+  info << "Hello world!";
+  return 0;
 }
