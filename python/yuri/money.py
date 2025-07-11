9be937e99fc59e1805cec96e2be37740517910cc
love-yuri <yuri2078170658@gmail.com>
2025-07-11 15:49:56 +0800

    fix: 修复文件路径转义字符，更新消费金额统计逻辑

--- python/yuri/money.py
+++ python/yuri/money.py
@@ -1,7 +1,7 @@
 import re
 
-with open("E:\love-yuri\yuri2078\python\yuri\money.txt", "r") as file:
-  
+with open("E:\\love-yuri\\yuri2078\\python\\yuri\\money.txt", "r") as file:
+
   count = {}
   for line in file.readlines():
     regex = re.compile(r"消费金额[^\d]*(\d+)食堂消费")
@@ -10,11 +10,12 @@ with open("E:\love-yuri\yuri2078\python\yuri\money.txt", "r") as file:
       money = (int)(match.group(1))
       if (count.get(money)):
         count[money] = count[money] + 1
-      else :
+      else:
         count.setdefault(money, 1)
-      
+
   print(f"""
+    30元消费: {count[30]} 次， 共计 {30 * count[30]}元； 
     20元消费: {count[20]} 次， 共计 {20 * count[20]}元； 
     10元消费: {count[10]} 次， 共计 {10 * count[10]}元;
-    共计消费: {count[20] * 20 + count[10] * 10} 元;
+    共计消费: {count[30] * 30 + count[20] * 20 + count[10] * 10} 元;
     """)
